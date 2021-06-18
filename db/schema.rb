d9849c1854822a416f0051901ecb3749d8aa05aa
KobayashiItto <kobayashi.itto110@gmail.com>
2021-06-18 21:05:16 +0900

    like機能実装中

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_110248) do
+ActiveRecord::Schema.define(version: 2021_06_18_103208) do
 
   create_table "comments", force: :cascade do |t|
-    t.text "body"
-    t.integer "post_id", null: false
+    t.string "content"
     t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -23,10 +23,12 @@ ActiveRecord::Schema.define(version: 2021_06_08_110248) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "post_id_id", null: false
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id_id"], name: "index_likes_on_post_id_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -53,5 +55,6 @@ ActiveRecord::Schema.define(version: 2021_06_08_110248) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "likes", "post_ids"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
 end
